51d719d673a1ea34aca2af1f0a81f72438ddf9ee
tearsqwe <chernyakpavel44@gmail.com>
2024-09-30 20:54:35 +0300

    added listener for moving to create activity

--- app/src/main/java/com/example/gittest2/MainActivity.kt
+++ app/src/main/java/com/example/gittest2/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.gittest2
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val btnCreate: Button = findViewById(R.id.button)
+        val btnRead: Button = findViewById(R.id.button2)
+        val btnUpdate: Button = findViewById(R.id.button3)
+        val btnDelete: Button = findViewById(R.id.button4)
+
+        btnCreate.setOnClickListener {
+            val intent= Intent(this,CreateActivity::class.java )
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
